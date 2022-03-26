610de8fbec928b507986bbd729edde298ce6f167
Mark <maropper@kiltandblade.com>
2022-03-26 16:13:51 +0000

    Add stb, use instead of ppm

--- RTIAW/RTIAW.cpp
+++ RTIAW/RTIAW.cpp
@@ -1,14 +1,48 @@
+#include <vector>
+#include <chrono>
+#define STBI_MSC_SECURE_CRT
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 #include "glm-format.hpp"
+#include "common.hpp"
+#include "fmt/chrono.h"
+
+struct Pixel3
+{
+  Pixel3(uint8 _r, uint8 _g, uint8 _b)
+    : r(_r)
+    , g(_g)
+    , b(_b)
+  {}
+  uint8 r, g, b;
+};
 
 int main()
 {
-  fmt::print("Hello, World!\n");
+  const int32 imageWidth = 256;
+  const int32 imageHeight = 256;
 
-  glm::vec3 a(1.f, -1.f, 2.5f);
-  glm::vec3 b(-9.f, 2.f, 42.f);
-  glm::vec3 c = a + b;
+  std::vector<Pixel3> imgBuffer;
+  imgBuffer.reserve(imageWidth * imageHeight);
 
-  fmt::print("{} + {} = {}", a, b, c);
+  const int32 maxY = imageHeight - 1;
+  const int32 maxX = imageWidth - 1;
+  for (int32 y = maxY; y >= 0; --y)
+  {
+    for (int32 x = 0; x < imageWidth; ++x)
+    {
+      real r = real(x) / maxX;
+      real g = real(y) / maxY;
+      real b = real(0.25);
+
+      constexpr real m = 256 - KindaSmallEpsilon;
+      imgBuffer.emplace(end(imgBuffer), uint8(m * r), uint8(m * g), uint8(m * b));
+    }
+  }
+
+  auto timeNow = std::chrono::system_clock::now();
+  std::string filename = fmt::format("rtiaw-{:%d-%H-%M-%S}.png", timeNow);
+  stbi_write_png(filename.c_str(), imageWidth, imageHeight, 3, imgBuffer.data(), 3 * imageWidth);
 
   return 0;
 }
---
+++ RTIAW/common.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <cstdint>
+
+using uint8 = std::uint8_t;
+using uint16 = std::uint16_t;
+using uint32 = std::uint32_t;
+using uint64 = std::uint64_t;
+
+using int8 = std::int8_t;
+using int16 = std::int16_t;
+using int32 = std::int32_t;
+using int64 = std::int64_t;
+
+#ifdef RT_USE_DOUBLE
+using real = double;
+#else
+using real = float;
+#endif // RT_USE_DOUBLE
+
+constexpr real SmallEpsilon = real(1e-8);
+constexpr real KindaSmallEpsilon = real(1e-4);
