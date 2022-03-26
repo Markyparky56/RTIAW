5ee00a0049e7f1feba1e07d6ba1cda60552a84db
Mark <maropper@kiltandblade.com>
2022-03-26 13:55:59 +0000

    Test fmt/glm format header

--- RTIAW/RTIAW.cpp
+++ RTIAW/RTIAW.cpp
@@ -1,8 +1,14 @@
-#define FMT_HEADER_ONLY
-#include <fmt/core.h>
+#include "glm-format.hpp"
 
 int main()
 {
   fmt::print("Hello, World!\n");
+
+  glm::vec3 a(1.f, -1.f, 2.5f);
+  glm::vec3 b(-9.f, 2.f, 42.f);
+  glm::vec3 c = a + b;
+
+  fmt::print("{} + {} = {}", a, b, c);
+
   return 0;
 }
